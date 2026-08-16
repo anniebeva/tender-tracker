11ffa669ca38e4c134dc4b77ae45f55d0851b4a8
anniebeva <anniebeva@gmail.com>
2026-08-16 16:17:14 +0300

    feat: add admin panel

--- tenders/admin.py
+++ tenders/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tender, TenderStatusHistory
+
+
+@admin.register(Tender)
+class TenderAdmin(admin.ModelAdmin):
+    """Configure tender administration"""
+
+    list_display = (
+        "title",
+        "status",
+        "created_by",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("status", "created_at")
+    search_fields = ("title", "description")
+    readonly_fields = ("created_at", "updated_at")
+
+
+@admin.register(TenderStatusHistory)
+class TenderStatusHistoryAdmin(admin.ModelAdmin):
+    """Configure tender status history administration"""
+
+    list_display = (
+        "tender",
+        "old_status",
+        "new_status",
+        "changed_by",
+        "changed_at",
+    )
+    list_filter = ("old_status", "new_status", "changed_at")
+    search_fields = (
+        "tender__title",
+        "reason",
+        "changed_by__username",
+    )
+    readonly_fields = ("changed_at",)
